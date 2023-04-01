c02b3620e7bd5e58943692d97ba5253b96f70b71
dhanush07-star <gurramdhanush21@gmail.com>
2023-04-01 16:27:35 +0530

    Loading State on cards

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import apiClient from '../services/api-client';
 
 
+
 export interface Platform{
   id:number;
   name:string;
@@ -25,19 +26,23 @@ interface FetchGamesResponse {
 export default function useGames() {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading,setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true)
     apiClient
       .get<FetchGamesResponse>("/games",{signal:controller.signal})
-      .then((res) => setGames(res.data.results))
+      .then((res) => {setGames(res.data.results)
+      setLoading(false)})
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message)
+        setLoading(false)
       });
 
       return ()=> controller.abort();
   },[]);
 
-  return {games, setGames,error, setError}
+  return {games, setGames,error, setError,isLoading}
 }
