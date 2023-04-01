d283e8863f3e15aec67301af7aec0781af3f20d4
dhanush07-star <gurramdhanush21@gmail.com>
2023-04-01 21:15:36 +0530

    display genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ export default function GameGrid() {
     <>
       {error && <Text>{error}</Text>}
       
-      <SimpleGrid columns={{sm:1,md:3,lg:3,xl:5}} padding='10px' spacing={10} >
+      <SimpleGrid columns={{sm:1,md:3,lg:3,xl:5}} padding='10px' spacing={3} >
         {isLoading && Skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton}  /></GameCardContainer>)}
         {data.map(game => <GameCardContainer>
           <GameCard key={game.id} game={game} />
