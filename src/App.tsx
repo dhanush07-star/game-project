2aaaffb9c55a99fc7d11a0d3013111806f691606
dhanush07-star <gurramdhanush21@gmail.com>
2023-04-01 12:23:07 +0530

    chakra added

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+   <div>
+      <Button colorScheme='blue'>Button</Button>
+   </div>
+  )
+}
+
+export default App
